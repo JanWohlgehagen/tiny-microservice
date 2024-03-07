2409568f68bfab8d1a6fd09405c5ca827797495a
Theut94 <Mikkeltheut@gmail.com>
2024-03-07 13:31:49 +0100

    daily push

---
+++ Tiny-Microservice/APIGateway/Controllers/SearchController.cs
@@ -0,0 +1,36 @@
+﻿using APIGateway.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+
+namespace APIGateway.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SearchController : ControllerBase
+    {
+        [HttpPost("Search")]
+        public async Task<IActionResult> search(string searchword, PostUserDTO userDto)
+        {
+            var AuthClient = new HttpClient();
+            var AuthRequestMessage = new HttpRequestMessage(HttpMethod.Get, "http://localhost:3001/Identity/Authenticate");
+
+            var client = new HttpClient();
+            var requestMessage = new HttpRequestMessage(HttpMethod.Get, $"http://localhost:3003/SearchUserController/get?searchstring{searchword}");
+
+            var request = await client.SendAsync(requestMessage);
+            var authRequest = await AuthClient.SendAsync(AuthRequestMessage);
+            var result = bool.Parse(await authRequest.Content.ReadAsStringAsync());
+            if (request.EnsureSuccessStatusCode() != null)
+            {
+                if (result)
+                {
+                    return Ok();
+                }
+                return Unauthorized();
+                
+            }
+            return BadRequest();
+        }
+
+    }
+}
---
+++ Tiny-Microservice/APIGateway/Controllers/UserController.cs
@@ -0,0 +1,56 @@
+﻿using APIGateway.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+
+namespace APIGateway.Controllers;
+
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : ControllerBase
+    {
+    [HttpPost("CreateUser")]
+    public async Task<IActionResult> PostUser(PostUserDTO userDto)
+    {
+        var client = new HttpClient();
+        var requestMessage = new HttpRequestMessage(HttpMethod.Post,"http://localhost:3003/User/JANSKRIVNOGETHER");
+        requestMessage.Content = new StringContent(JsonSerializer.Serialize(userDto));
+
+        var request = await client.SendAsync(requestMessage);
+        if(request.EnsureSuccessStatusCode() != null)
+        {
+            return Ok();
+        }
+        return BadRequest();
+    }
+
+    [HttpPut("EditUser")]
+    public async Task<IActionResult> EditUser(EditUserDTO userDto)
+    {
+        var client = new HttpClient();
+        var requestMessage = new HttpRequestMessage(HttpMethod.Put, "http://localhost:3003/User/EditUser");
+        requestMessage.Content = new StringContent(JsonSerializer.Serialize(userDto));
+
+        var request = await client.SendAsync(requestMessage);
+        if (request.EnsureSuccessStatusCode() != null)
+        {
+            return Ok();
+        }
+        return BadRequest();
+    }
+
+    [HttpPut("EditUserSettings")]
+    public async Task<IActionResult> EditSettings(UserSettingsDTO userDto, string Id)
+    {
+        var client = new HttpClient();
+        var requestMessage = new HttpRequestMessage(HttpMethod.Put, $"http://localhost:3003/User/EditUserSettings?Id={Id}");
+        requestMessage.Content = new StringContent(JsonSerializer.Serialize(userDto));
+
+        var request = await client.SendAsync(requestMessage);
+        if (request.EnsureSuccessStatusCode() != null)
+        {
+            return Ok();
+        }
+        return BadRequest();
+    }
+}
+
