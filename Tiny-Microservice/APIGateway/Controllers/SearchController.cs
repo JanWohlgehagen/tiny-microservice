e06c036342c09f9f70e41559348a460566cd2ce4
Theut94 <Mikkeltheut@gmail.com>
2024-03-21 13:22:38 +0100

    chore: try and fix api's

--- Tiny-Microservice/APIGateway/Controllers/SearchController.cs
+++ Tiny-Microservice/APIGateway/Controllers/SearchController.cs
@@ -1,6 +1,5 @@
-﻿using APIGateway.Models;
-using Microsoft.AspNetCore.Mvc;
-using System.Text.Json;
+﻿using Microsoft.AspNetCore.Mvc;
+using SharedModels;
 
 namespace APIGateway.Controllers
 {
@@ -9,13 +8,13 @@ namespace APIGateway.Controllers
     public class SearchController : ControllerBase
     {
         [HttpPost("Search")]
-        public async Task<IActionResult> search(string searchword, PostUserDTO userDto)
+        public async Task<IActionResult> search(string searchword, UserSimpleDTO userDto)
         {
             var AuthClient = new HttpClient();
-            var AuthRequestMessage = new HttpRequestMessage(HttpMethod.Get, "http://localhost:3001/Identity/Authenticate");
+            var AuthRequestMessage = new HttpRequestMessage(HttpMethod.Get, "http://Identity:3001/Identity/Authenticate");
 
             var client = new HttpClient();
-            var requestMessage = new HttpRequestMessage(HttpMethod.Get, $"http://localhost:3003/SearchUserController/get?searchstring{searchword}");
+            var requestMessage = new HttpRequestMessage(HttpMethod.Get, $"http://search:3003/SearchUser/FindUser?searchstring{searchword}");
 
             var request = await client.SendAsync(requestMessage);
             var authRequest = await AuthClient.SendAsync(AuthRequestMessage);
