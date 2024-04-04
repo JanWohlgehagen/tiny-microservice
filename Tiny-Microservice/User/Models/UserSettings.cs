3a0f90c396f14c7a8a0c58f6c7e82f4b53e3fe59
JanWohlgehagen <jan.wohlgehagen@gmail.com>
2024-04-04 11:38:33 +0200

    fix:fixed the user db context

--- Tiny-Microservice/User/Models/UserSettings.cs
+++ Tiny-Microservice/User/Models/UserSettings.cs
@@ -4,6 +4,8 @@ public class UserSettings
 {
     public string id { get; set; }
     public string userId { get; set; }
+    
+    public User User { get; set; }
     public string theme { get; set; }
     public string language { get; set; }
 }
